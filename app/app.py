bd352aa64c07e5ea6c7c473c90659f71df162d37
magoaml <magomed.amliev@edu.fh-joanneum.at>
2025-06-01 20:25:44 +0200

    test7.0

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import redis
+import os
 
 app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379, decode_responses=True)
@@ -14,6 +15,10 @@ def write():
 def read():
     data = r.get("key")
     return jsonify({"data": data})
+
+@app.route("/")
+def index():
+    return send_from_directory(os.path.join(app.root_path, "static"), "test.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
